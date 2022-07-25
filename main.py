4d243ac3d737e9325aadb529d52e7dee86e3e3f3
Dunkm4st3r <hartmanneric.mail@gmail.com>
2022-07-25 13:27:05 +0200

    Prototyp zum testen

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 # Eingabe der Amagramme
+anagramm1 = list(input("Anagramm1: ").lower())
+anagramm2 = list(input("Anagramm2: ").lower())
+
+# x iteriert durch die Buchstaben von anagram1
+for x in anagramm1:
+    # y durch die Buchstaben von anagram2
+    for y in anagramm2:
+        if x == y:
+            anagramm2.remove(x)
+        else:
+            pass
+
+if len(anagramm2) > 0:
+    print("Kein Anagramm des jeweils anderen Wortes.")
+else:
+    print(" Anagramm des jeweils anderen Wortes!")
