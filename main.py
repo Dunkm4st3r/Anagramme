a8fc026c3e5d6ca4e18b63c80b6b8086ff1b40c0
Dunkm4st3r <hartmanneric.mail@gmail.com>
2022-07-25 13:57:42 +0200

    String länge überprüft

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-# Eingabe der Amagramme
+# Eingabe der Anagramme
 anagramm1 = list(input("Anagramm1: ").lower())
 anagramm2 = list(input("Anagramm2: ").lower())
 
-# x iteriert durch die Buchstaben von anagram1
-for x in anagramm1:
-    # y durch die Buchstaben von anagram2
-    for y in anagramm2:
-        if x == y:
-            anagramm2.remove(x)
-        else:
-            pass
+if len(anagramm1) == len(anagramm2):
+    # x iteriert durch die Buchstaben von anagramm1
+    for x in anagramm1:
+        # y durch die Buchstaben von anagramm2
+        for y in anagramm2:
+            # wenn ein gleicher Buchstabe gefunden wurde, dann lösche ihn aus dem zweiten Anagramm
+            if x == y:
+                # Buchstabe löschen
+                anagramm2.remove(x)
+            else:
+                pass
 
+# Wenn mind. ein Buchstrabe übrig geblieben ist, dann ist es kein Anagramm
 if len(anagramm2) > 0:
     print("Kein Anagramm des jeweils anderen Wortes.")
 else:
-    print(" Anagramm des jeweils anderen Wortes!")
+    print("Anagramm des jeweils anderen Wortes!")
